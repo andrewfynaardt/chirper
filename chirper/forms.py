a80a2f2161c377c09aeaa31e0092adbc34ca8f70
Lin2025-888 <s96024n@gmail.com>
2025-02-16 10:54:24 -0600

    Created the form and view to accept user chirp input.

---
+++ chirper/forms.py
@@ -0,0 +1,19 @@
+"""
+forms.py
+Created: 2025-2-16
+Updated: 2025-2-16
+
+Contains the form to accept user chirp input. 
+"""
+
+from django import forms
+from .models import Chirps
+
+
+class ChirpForm(forms.ModelForm):
+    """
+    Create the form based on the existing Chirps class.
+    Only allow the user to inout chirp_body.
+    """
+    model = Chirps
+    fields = ['chirp_body']
--- chirper/views.py
+++ chirper/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ChirpForm
 
 # Create your views here.
+def chirp_view(request):
+    """
+    The view to display the chirpForm to users.
+    """
+    
+    # Process the form data if it's a POST request
+    if request.method == 'POST':
+        # Create a form instance and fill in data gained from the request
+        form = ChirpForm(request.POST)
+        
+        # Save and redirect to success page if from is valid
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+
+    # Create blank form if a GET (or any other method)
+    else:
+        form = ChirpForm()
+
+    return render(request, 'chirp_form.html', {'form': form})
